87fb3a1c5d7bbdcece2380d72ac45b420e7c2aa7
Albert <gray.pig101@gmail.com>
2026-06-17 19:52:35 +0500

    2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.games.EvenGame;
 import hexlet.code.games.GCDGame;
+import hexlet.code.games.GreetGame;
 import hexlet.code.games.MathGame;
 import hexlet.code.games.PrimeGame;
 import hexlet.code.games.ProgressionGame;
@@ -12,7 +13,6 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\n");
         System.out.println("""
                 Please enter a game number and press Enter.\s
                 1 - Greet\s
@@ -27,29 +27,22 @@ public class App {
 
         switch (userChoice) {
             case "1":
-                Cli.helloUser();
-                break;
+                GreetGame.run();
             case "0":
-                System.exit(0);
-                break;
+                System.out.println("See you later!");
             case "2":
                 EvenGame.evenGameMain();
-                break;
             case "3":
                 MathGame.mathGameMain();
-                break;
             case "4":
                 GCDGame.gcdGameMain();
-                break;
             case "5":
                 ProgressionGame.progressionMain();
-                break;
             case "6":
                 PrimeGame.primeMain();
-                break;
             default:
                 System.out.println("Invalid input.");
-                break;
         }
+        scanner.close();
     }
 }
--- app/src/main/java/hexlet/code/games/GreetGame.java
+++ app/src/main/java/hexlet/code/games/GreetGame.java
@@ -1,15 +1,15 @@
-package hexlet.code;
+package hexlet.code.games;
 
 import java.util.Scanner;
 
-public class Cli {
-    public static void greet() {
+public class GreetGame {
+    public static void run() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
+        System.out.println("May I have your name? ");
         String name = scanner.nextLine();
         System.out.println("Hello, " + name + "!");
+        scanner.close();
     }
 }
-
